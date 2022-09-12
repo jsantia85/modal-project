ac9a5222bfffd7d2bd0ff7ea040d107969ce9f85
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-12 14:52:54 -0400

    add notes

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 'use strict';
 
+// elements
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
-
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
+// functions
 const addHidden = function() {
   modal.classList.add('hidden')
   overlay.classList.add('hidden')
@@ -16,11 +17,11 @@ const removeHidden = function() {
     overlay.classList.remove('hidden')
 }
 
+// loop for modals
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', removeHidden)
 }
 
-
+// event listener
 btnCloseModal.addEventListener('click', addHidden)
-
 overlay.addEventListener('click', addHidden)
