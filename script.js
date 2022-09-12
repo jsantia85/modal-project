df51a7ae5da3c8610c138b3f1ad19f0ad88914ed
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-12 13:08:25 -0400

    create const and assign it to all els with show-modal class

--- script.js
+++ script.js
@@ -3,3 +3,5 @@
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
+
+const btnsOpenModal = document.querySelectorAll('.show-modal')
