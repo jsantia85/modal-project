8b5cb81ee1e7e107d06ae209ea76e851bb428a32
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-12 13:11:21 -0400

    loop through show btns and display text content in the console.

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 
 const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent)
+}
